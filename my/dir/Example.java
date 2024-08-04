17f7a8ef662822b74e5a079f48e8b00321a710c1
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-08-05 00:32:20 +0300

    refactoring project

---
+++ my/dir/Example.java
@@ -0,0 +1,9 @@
+package my.dir;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(RandomUtils.nextInt(1, 1000));
+    }
+}
